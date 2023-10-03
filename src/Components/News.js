8fdb7d33a9b562511b05dd81d08996b6f4e3aa61
Raj kansagara <rajkansagara296@gmail.com>
2023-10-03 18:40:56 +0530

    add lables and date and time

--- src/Components/News.js
+++ src/Components/News.js
@@ -67,7 +67,7 @@ export class News extends Component {
         <div className='row'>
           {!this.state.loading && this.state.articles.map((element) => {
             return <div className='col-md-4' key={element.url} >
-              <NewsItems title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+              <NewsItems title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
             </div>
           })}
         </div>
--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react'
 
 export class NewsItems extends Component {
     render() {
-        let {title, description,imageUrl,newsUrl} = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className='conitainer my-3'>
                 <div className="card">
-                    <img src={!imageUrl?"https://t4.ftcdn.net/jpg/05/17/53/57/240_F_517535712_q7f9QC9X6TQxWi6xYZZbMmw5cnLMr279.jpg":imageUrl} className="card-img-top" alt="..." />
+                    <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: "50%",zIndex:1}}>
+                        {source}
+                    </span>
+                    <img src={!imageUrl ? "https://t4.ftcdn.net/jpg/05/17/53/57/240_F_517535712_q7f9QC9X6TQxWi6xYZZbMmw5cnLMr279.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href={newsUrl}  rel="noreferrer" target='_blank' className="btn btn-sm btn-primary">Read More</a>
+                        <p className="card-text"><small className="text-body-secondary">By {author} on {new Date(date).toUTCString()}</small></p>
+                        <a href={newsUrl} rel="noreferrer" target='_blank' className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
